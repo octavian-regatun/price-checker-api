fd26c474db2d480c569e7aa503e66464de868ed2
Octavian Regatun <octavian.regatun@outlook.com>
2020-06-28 01:48:07 +0300

    add support for PcGarage

---
+++ src/pcgarage.js
@@ -0,0 +1,49 @@
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+
+class PcGarage {
+  static URL = 'https://www.pcgarage.ro/cauta';
+
+  static #getProductData = ($products) => {
+    const products = [];
+    for (const $product of $products) {
+      const $ = cheerio.load($product);
+
+      const stock = $('.pb-price-container').children().eq(2).text();
+
+      if (stock.trim() !== 'Nu este in stoc') {
+        let price = $('.price').text();
+        price = price.replace(' RON', '');
+        price = price.replace('.', '');
+        price = price.replace(',', '.');
+        price = Number.parseFloat(price);
+
+        const name = $('.pb-name').eq(0).children().eq(0).attr('title');
+
+        // eslint-disable-next-line unicorn/prevent-abbreviations
+        const imgSrc = $('img').eq(0).attr('src');
+
+        products.push({ name, price, imgSrc });
+      }
+    }
+    return products;
+  };
+
+  static async request(search) {
+    let $;
+    try {
+      $ = await fetch(`${this.URL}/${search}`, { method: 'GET' })
+        .then((response) => response.text())
+        .then((body) => cheerio.load(body));
+    } catch (error) {
+      console.log(error);
+    }
+
+    const $products = $('.product-box-container').get();
+
+    const response = { products: this.#getProductData($products) };
+
+    return response;
+  }
+}
+module.exports = PcGarage;
