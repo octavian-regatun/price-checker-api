1eadbdd12dbf27aa9740b2bcdfd66ab5dfad8380
Octavian Regatun <octavian.regatun@outlook.com>
2020-07-11 17:10:15 +0300

    format code

--- src/pcgarage.js
+++ src/pcgarage.js
@@ -2,48 +2,48 @@ const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 
 class PcGarage {
-  static URL = 'https://www.pcgarage.ro/cauta';
+	static URL = 'https://www.pcgarage.ro/cauta';
 
-  static #getProductData = ($products) => {
-    const products = [];
-    for (const $product of $products) {
-      const $ = cheerio.load($product);
+	static #getProductData = ($products) => {
+		const products = [];
+		for (const $product of $products) {
+			const $ = cheerio.load($product);
 
-      const stock = $('.pb-price-container').children().eq(2).text();
+			const stock = $('.pb-price-container').children().eq(2).text();
 
-      if (stock.trim() !== 'Nu este in stoc') {
-        let price = $('.price').text();
-        price = price.replace(' RON', '');
-        price = price.replace('.', '');
-        price = price.replace(',', '.');
-        price = Number.parseFloat(price);
+			if (stock.trim() !== 'Nu este in stoc') {
+				let price = $('.price').text();
+				price = price.replace(' RON', '');
+				price = price.replace('.', '');
+				price = price.replace(',', '.');
+				price = Number.parseFloat(price);
 
-        const name = $('.pb-name').eq(0).children().eq(0).attr('title');
+				const name = $('.pb-name').eq(0).children().eq(0).attr('title');
 
-        // eslint-disable-next-line unicorn/prevent-abbreviations
-        const imgSrc = $('img').eq(0).attr('src');
+				const imgSrc = $('img').eq(0).attr('src');
 
-        products.push({ name, price, imgSrc });
-      }
-    }
-    return products;
-  };
+				products.push({ name, price, imgSrc });
+			}
+		}
 
-  static async request(search) {
-    let $;
-    try {
-      $ = await fetch(`${this.URL}/${search}`, { method: 'GET' })
-        .then((response) => response.text())
-        .then((body) => cheerio.load(body));
-    } catch (error) {
-      console.log(error);
-    }
+		return products;
+	};
 
-    const $products = $('.product-box-container').get();
+	static async request(search) {
+		let $;
+		try {
+			$ = await fetch(`${this.URL}/${search}`, { method: 'GET' })
+				.then((response) => response.text())
+				.then((body) => cheerio.load(body));
+		} catch (error) {
+			console.log(error);
+		}
 
-    const response = { products: this.#getProductData($products) };
+		const $products = $('.product-box-container').get();
 
-    return response;
-  }
+		const response = { products: this.#getProductData($products) };
+
+		return response;
+	}
 }
 module.exports = PcGarage;
