39a912b756f7e22a3e45e1b817fdba28d6e360f2
Octavian Regatun <31423936+octavian-regatun@users.noreply.github.com>
2020-07-14 01:23:19 +0300

    Implement requestAllPages method to PcGarage API (#13)
    * update .gitignore
    
    * update rule
    
    * add delay package
    
    * add requestAllPages method to PcGarage API
    
    * update changelog

--- src/pcgarage.js
+++ src/pcgarage.js
@@ -1,18 +1,82 @@
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
+const delay = require('delay');
 
 class PcGarage {
 	static URL = 'https://www.pcgarage.ro/cauta';
 
-	static #getHTML = async (search) => {
-		try {
-			const $ = await fetch(`${this.URL}/${search}`, { method: 'GET' })
-				.then((response) => response.text())
-				.then((body) => cheerio.load(body));
+	static #getHTMLForSearch = async (search, wantURL = false) => {
+		let url;
+		const $ = await fetch(`${this.URL}/${search}`, { method: 'GET' })
+			.then((response) => {
+				url = response.url;
+				return response.text();
+			})
+			.then((body) => cheerio.load(body));
 
-			return $;
-		} catch (error) {
-			console.log(error);
+		if (wantURL) {
+			return { $, url };
+		}
+
+		return $;
+	};
+
+	static #getHTMLForPage = async (URL, page) => {
+		const $ = await fetch(`${URL}/pagina${page}`, { method: 'GET' })
+			.then((response) => response.text())
+			.then((body) => cheerio.load(body));
+
+		return $;
+	};
+
+	static #isPageNumberButton = ($, pageNumber) => {
+		if (pageNumber < 2 || pageNumber > 5) {
+			throw new Error('The page number has to be between 2 and 5 (including)');
+		} else {
+			return (
+				parseInt(
+					$('.pagination')
+						.first()
+						.children()
+						.eq(pageNumber - 1)
+						.text(),
+					10
+				) === pageNumber
+			);
+		}
+	};
+
+	static #howManyPagesAre = ($) => {
+		if (this.#isPageNumberButton($, 5)) {
+			// Can be either 5 or more
+			// How much page numbers really are?
+			let lastPageNumber = $('.pagination')
+				.first()
+				.children()
+				.last()
+				.children()
+				.attr('href');
+
+			lastPageNumber = lastPageNumber.split('pagina')[1];
+			lastPageNumber = lastPageNumber.replace('/', '');
+
+			return parseInt(lastPageNumber, 10);
+		}
+
+		if (this.#isPageNumberButton($, 4)) {
+			return 4;
+		}
+
+		if (this.#isPageNumberButton($, 3)) {
+			return 3;
+		}
+
+		if (this.#isPageNumberButton($, 2)) {
+			return 2;
+		}
+
+		if (this.#isPageNumberButton($, 1)) {
+			return 1;
 		}
 	};
 
@@ -28,7 +92,7 @@ class PcGarage {
 				price = price.replace(' RON', '');
 				price = price.replace('.', '');
 				price = price.replace(',', '.');
-				price = Number.parseFloat(price);
+				price = parseFloat(price);
 
 				const name = $('.product_box_name').children().attr('title');
 
@@ -48,18 +112,39 @@ class PcGarage {
 		return products;
 	};
 
-	static #isPageNumberButton = async ($) => {
-		console.log($('.pagination').first().children().length);
-	};
+	static async requestAllPages(search) {
+		const { $, url: URL } = await this.#getHTMLForSearch(search, true);
 
-	static async requestFirstPage(search) {
-		const $ = await this.#getHTML(search);
+		await delay(5000);
 
-		const $products = $('.product_box_container').get();
+		const totalNumberOfPages = this.#howManyPagesAre($);
 
-		const response = { products: this.#getProductData($products) };
+		let response = [];
+
+		for (let i = 1; i <= totalNumberOfPages; i++) {
+			const $ = await this.#getHTMLForPage(URL, i);
+
+			const $products = $('.product_box_container').get();
+
+			response.push({ page: i, products: this.#getProductData($products) });
+
+			console.log(`Products from page ${i} have been loaded!`);
+
+			await delay(5000);
+		}
 
 		return response;
 	}
+
+	static async requestFirstPage(search) {
+		const $ = await this.#getHTMLForSearch(search);
+
+		const $products = $('.product_box_container').get();
+
+		console.log(`Products have been loaded!`);
+
+		return { products: this.#getProductData($products) };
+	}
 }
+
 module.exports = PcGarage;
